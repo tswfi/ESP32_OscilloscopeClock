a3ef04b2513460abdad8edbf397d4ebd85e471d2
Tatu Wikman <tatu.wikman@gmail.com>
2019-04-19 18:44:14 +0300

    started adding wifimanager

--- src/ESP32_OscilloscopeClock_v1.0.cpp
+++ src/ESP32_OscilloscopeClock_v1.0.cpp
@@ -68,13 +68,18 @@
 #include <simpleDSTadjust.h>
 #include <WiFi.h>
 
+#include <DNSServer.h>
+#include <WebServer.h>
+#include <WiFiManager.h>
 
 //#define EXCEL
 //#define NTP
-#define UTC_OFFSET -8
+#define UTC_OFFSET 0
 struct dstRule StartRule = {"PDT", Second, Sun, Mar, 2, 3600}; // Pacific Daylight time = UTC/GMT -7 hours
 struct dstRule EndRule = {"PST", First, Sun, Nov, 1, 0};       // Pacific Standard time = UTC/GMT -8 hour
 simpleDSTadjust dstAdjusted(StartRule, EndRule);
+
+WiFiManager wifiManager;
 
 // change for different NTP (time servers)
 #define NTP_SERVERS "us.pool.ntp.org", "time.nist.gov", "pool.ntp.org"
@@ -83,18 +88,13 @@ simpleDSTadjust dstAdjusted(StartRule, EndRule);
 #define NTP_MIN_VALID_EPOCH 1533081600
 
 //Variables
-int           lastx,lasty;
-unsigned long currentMillis  = 0;
+int lastx, lasty;
+unsigned long currentMillis = 0;
 unsigned long previousMillis = 0;
-int           Timeout        = 20*1000;
-const    long interval       = 10*60*1000; //milliseconds, you should twick this
-                                          //to get a better accuracy
-
-char *ssid      = "SSID";        // Set you WiFi SSID
-char *password  = "PASSWORD";        // Set you WiFi password
-
+int Timeout = 20 * 1000;
+const long interval = 10 * 60 * 1000; //milliseconds, you should twick this
+                                      //to get a better accuracy
 const int TRIGGER = 15;
-
 
 //*****************************************************************************
 // Dot
@@ -102,44 +102,48 @@ const int TRIGGER = 15;
 
 inline void Dot(int x, int y)
 {
-    if (lastx!=x){
-      lastx=x;
-      dac_output_voltage(DAC_CHANNEL_1, x);
-    }
-    #if defined EXCEL
-      Serial.print("0x");
-      if (x<=0xF) Serial.print("0");
-      Serial.print(x,HEX);
-      Serial.print(",");
-    #endif
-    #if defined EXCEL
-      Serial.print("0x");
-      if (lasty<=0xF) Serial.print("0");
-      Serial.print(lasty,HEX);
-      Serial.println(",");
-    #endif
-    if (lasty!=y){
-      lasty=y;
-      dac_output_voltage(DAC_CHANNEL_2, y);
-    }
-    #if defined EXCEL
-      Serial.print("0x");
-      if (x<=0xF) Serial.print("0");
-      Serial.print(x,HEX);
-      Serial.print(",");
-    #endif
-    #if defined EXCEL
-      Serial.print("0x");
-      if (y<=0xF) Serial.print("0");
-      Serial.print(y,HEX);
-      Serial.println(",");
-    #endif
+  if (lastx != x)
+  {
+    lastx = x;
+    dac_output_voltage(DAC_CHANNEL_1, x);
+  }
+#if defined EXCEL
+  Serial.print("0x");
+  if (x <= 0xF)
+    Serial.print("0");
+  Serial.print(x, HEX);
+  Serial.print(",");
+#endif
+#if defined EXCEL
+  Serial.print("0x");
+  if (lasty <= 0xF)
+    Serial.print("0");
+  Serial.print(lasty, HEX);
+  Serial.println(",");
+#endif
+  if (lasty != y)
+  {
+    lasty = y;
+    dac_output_voltage(DAC_CHANNEL_2, y);
+  }
+#if defined EXCEL
+  Serial.print("0x");
+  if (x <= 0xF)
+    Serial.print("0");
+  Serial.print(x, HEX);
+  Serial.print(",");
+#endif
+#if defined EXCEL
+  Serial.print("0x");
+  if (y <= 0xF)
+    Serial.print("0");
+  Serial.print(y, HEX);
+  Serial.println(",");
+#endif
 }
 
 // End Dot
 //*****************************************************************************
-
-
 
 //*****************************************************************************
 // Line
@@ -149,171 +153,196 @@ inline void Dot(int x, int y)
 
 void Line(byte x1, byte y1, byte x2, byte y2)
 {
-    int n = 2;
-    int acc;
-    bool first_point = true;
-    // for speed, there are 8 DDA's, one for each octant
-    if (y1 < y2) { // quadrant 1 or 2
-        byte dy = y2 - y1;
-        if (x1 < x2) { // quadrant 1
-            byte dx = x2 - x1;
-            if (dx > dy) { // < 45
-                acc = (dx >> 1);
-                for (; x1 <= x2; x1++) {
-                    Dot(x1, y1);
-                    if (first_point)
-                    {
-                      delayMicroseconds(n);
-                      first_point = false;
-                      digitalWrite(TRIGGER, LOW);
-                    }
-                    acc -= dy;
-                    if (acc < 0) {
-                        y1++;
-                        acc += dx;
-                    }
-                }
-            }
-            else {   // > 45
-                acc = dy >> 1;
-                for (; y1 <= y2; y1++) {
-                    Dot(x1, y1);
-                    if (first_point)
-                    {
-                      delayMicroseconds(n);
-                      first_point = false;
-                      digitalWrite(TRIGGER, LOW);
-                    }
-                    acc -= dx;
-                    if (acc < 0) {
-                        x1++;
-                        acc += dy;
-                    }
-                }
-            }
+  int n = 2;
+  int acc;
+  bool first_point = true;
+  // for speed, there are 8 DDA's, one for each octant
+  if (y1 < y2)
+  { // quadrant 1 or 2
+    byte dy = y2 - y1;
+    if (x1 < x2)
+    { // quadrant 1
+      byte dx = x2 - x1;
+      if (dx > dy)
+      { // < 45
+        acc = (dx >> 1);
+        for (; x1 <= x2; x1++)
+        {
+          Dot(x1, y1);
+          if (first_point)
+          {
+            delayMicroseconds(n);
+            first_point = false;
+            digitalWrite(TRIGGER, LOW);
+          }
+          acc -= dy;
+          if (acc < 0)
+          {
+            y1++;
+            acc += dx;
+          }
         }
-        else {  // quadrant 2
-            byte dx = x1 - x2;
-            if (dx > dy) { // < 45
-                acc = dx >> 1;
-                for (; x1 >= x2; x1--) {
-                    Dot(x1, y1);
-                    if (first_point)
-                    {
-                      delayMicroseconds(n);
-                      first_point = false;
-                      digitalWrite(TRIGGER, LOW);
-                    }
-                    acc -= dy;
-                    if (acc < 0) {
-                        y1++;
-                        acc += dx;
-                    }
-                }
-            }
-            else {  // > 45
-                acc = dy >> 1;
-                for (; y1 <= y2; y1++) {
-                    Dot(x1, y1);
-                    if (first_point)
-                    {
-                      delayMicroseconds(n);
-                      first_point = false;
-                      digitalWrite(TRIGGER, LOW);
-                    }
-                    acc -= dx;
-                    if (acc < 0) {
-                        x1--;
-                        acc += dy;
-                    }
-                }
-            }
+      }
+      else
+      { // > 45
+        acc = dy >> 1;
+        for (; y1 <= y2; y1++)
+        {
+          Dot(x1, y1);
+          if (first_point)
+          {
+            delayMicroseconds(n);
+            first_point = false;
+            digitalWrite(TRIGGER, LOW);
+          }
+          acc -= dx;
+          if (acc < 0)
+          {
+            x1++;
+            acc += dy;
+          }
         }
+      }
     }
-    else { // quadrant 3 or 4
-        byte dy = y1 - y2;
-        if (x1 < x2) { // quadrant 4
-            byte dx = x2 - x1;
-            if (dx > dy) {  // < 45
-                acc = dx >> 1;
-                for (; x1 <= x2; x1++) {
-                    Dot(x1, y1);
-                    if (first_point)
-                    {
-                      delayMicroseconds(n);
-                      first_point = false;
-                      digitalWrite(TRIGGER, LOW);
-                    }
-                    acc -= dy;
-                    if (acc < 0) {
-                        y1--;
-                        acc += dx;
-                    }
-                }
-
-            }
-            else {  // > 45
-                acc = dy >> 1;
-                for (; y1 >= y2; y1--) {
-                    Dot(x1, y1);
-                    if (first_point)
-                    {
-                      delayMicroseconds(n);
-                      first_point = false;
-                      digitalWrite(TRIGGER, LOW);
-                    }
-                    acc -= dx;
-                    if (acc < 0) {
-                        x1++;
-                        acc += dy;
-                    }
-                }
-
-            }
+    else
+    { // quadrant 2
+      byte dx = x1 - x2;
+      if (dx > dy)
+      { // < 45
+        acc = dx >> 1;
+        for (; x1 >= x2; x1--)
+        {
+          Dot(x1, y1);
+          if (first_point)
+          {
+            delayMicroseconds(n);
+            first_point = false;
+            digitalWrite(TRIGGER, LOW);
+          }
+          acc -= dy;
+          if (acc < 0)
+          {
+            y1++;
+            acc += dx;
+          }
         }
-        else {  // quadrant 3
-            byte dx = x1 - x2;
-            if (dx > dy) { // < 45
-                acc = dx >> 1;
-                for (; x1 >= x2; x1--) {
-                    Dot(x1, y1);
-                    if (first_point)
-                    {
-                      delayMicroseconds(n);
-                      first_point = false;
-                      digitalWrite(TRIGGER, LOW);
-                    }
-                    acc -= dy;
-                    if (acc < 0) {
-                        y1--;
-                        acc += dx;
-                    }
-                }
-
-            }
-            else {  // > 45
-                acc = dy >> 1;
-                for (; y1 >= y2; y1--) {
-                    Dot(x1, y1);
-                    if (first_point)
-                    {
-                      delayMicroseconds(n);
-                      first_point = false;
-                      digitalWrite(TRIGGER, LOW);
-                    }
-                    acc -= dx;
-                    if (acc < 0) {
-                        x1--;
-                        acc += dy;
-                    }
-                }
-            }
+      }
+      else
+      { // > 45
+        acc = dy >> 1;
+        for (; y1 <= y2; y1++)
+        {
+          Dot(x1, y1);
+          if (first_point)
+          {
+            delayMicroseconds(n);
+            first_point = false;
+            digitalWrite(TRIGGER, LOW);
+          }
+          acc -= dx;
+          if (acc < 0)
+          {
+            x1--;
+            acc += dy;
+          }
         }
-
+      }
     }
+  }
+  else
+  { // quadrant 3 or 4
+    byte dy = y1 - y2;
+    if (x1 < x2)
+    { // quadrant 4
+      byte dx = x2 - x1;
+      if (dx > dy)
+      { // < 45
+        acc = dx >> 1;
+        for (; x1 <= x2; x1++)
+        {
+          Dot(x1, y1);
+          if (first_point)
+          {
+            delayMicroseconds(n);
+            first_point = false;
+            digitalWrite(TRIGGER, LOW);
+          }
+          acc -= dy;
+          if (acc < 0)
+          {
+            y1--;
+            acc += dx;
+          }
+        }
+      }
+      else
+      { // > 45
+        acc = dy >> 1;
+        for (; y1 >= y2; y1--)
+        {
+          Dot(x1, y1);
+          if (first_point)
+          {
+            delayMicroseconds(n);
+            first_point = false;
+            digitalWrite(TRIGGER, LOW);
+          }
+          acc -= dx;
+          if (acc < 0)
+          {
+            x1++;
+            acc += dy;
+          }
+        }
+      }
+    }
+    else
+    { // quadrant 3
+      byte dx = x1 - x2;
+      if (dx > dy)
+      { // < 45
+        acc = dx >> 1;
+        for (; x1 >= x2; x1--)
+        {
+          Dot(x1, y1);
+          if (first_point)
+          {
+            delayMicroseconds(n);
+            first_point = false;
+            digitalWrite(TRIGGER, LOW);
+          }
+          acc -= dy;
+          if (acc < 0)
+          {
+            y1--;
+            acc += dx;
+          }
+        }
+      }
+      else
+      { // > 45
+        acc = dy >> 1;
+        for (; y1 >= y2; y1--)
+        {
+          Dot(x1, y1);
+          if (first_point)
+          {
+            delayMicroseconds(n);
+            first_point = false;
+            digitalWrite(TRIGGER, LOW);
+          }
+          acc -= dx;
+          if (acc < 0)
+          {
+            x1--;
+            acc += dy;
+          }
+        }
+      }
+    }
+  }
 
-
-    digitalWrite(TRIGGER, HIGH);
+  digitalWrite(TRIGGER, HIGH);
 }
 
 // End Line
@@ -325,38 +354,54 @@ void Line(byte x1, byte y1, byte x2, byte y2)
 
 void PlotTable(byte *SubTable, int SubTableSize, int skip, int opt, int offset)
 {
-  int i=offset;
-  while (i<SubTableSize){
-    if (SubTable[i+2]==skip){
-      i=i+3;
-      if (opt==1) if (SubTable[i]==skip) i++;
+  int i = offset;
+  while (i < SubTableSize)
+  {
+    if (SubTable[i + 2] == skip)
+    {
+      i = i + 3;
+      if (opt == 1)
+        if (SubTable[i] == skip)
+          i++;
     }
-    Line(SubTable[i],SubTable[i+1],SubTable[i+2],SubTable[i+3]);
-    if (opt==2){
-      Line(SubTable[i+2],SubTable[i+3],SubTable[i],SubTable[i+1]);
+    Line(SubTable[i], SubTable[i + 1], SubTable[i + 2], SubTable[i + 3]);
+    if (opt == 2)
+    {
+      Line(SubTable[i + 2], SubTable[i + 3], SubTable[i], SubTable[i + 1]);
     }
-    i=i+2;
-    if (SubTable[i+2]==0xFF) break;
+    i = i + 2;
+    if (SubTable[i + 2] == 0xFF)
+      break;
   }
 }
 
 // End PlotTable
 //*****************************************************************************
 
-
 // time_t getNtpTime()
-void getNtpTime() {
+void getNtpTime()
+{
   time_t now;
 
   int i = 0;
   configTime(UTC_OFFSET * 3600, 0, NTP_SERVERS);
-  while((now = time(nullptr)) < NTP_MIN_VALID_EPOCH) {
+  while ((now = time(nullptr)) < NTP_MIN_VALID_EPOCH)
+  {
     delay(500);
     i++;
     if (i > 60)
       break;
   }
 }
+
+
+void configModeCallback (WiFiManager *myWiFiManager) {
+  Serial.println("Entered config mode");
+  Serial.println(WiFi.softAPIP());
+  // TODO:
+  Serial.println(myWiFiManager->getConfigPortalSSID());
+}
+
 
 //*****************************************************************************
 // setup
@@ -373,19 +418,16 @@ void setup()
 
   Serial.println("Connecting to Wi-Fi");
 
+  // for showing text "please connect to access point XXX to configure"
+  wifiManager.setAPCallback(configModeCallback);
+  // timezone
+  WiFiManagerParameter timezoneparameter("timezone", "+2, -8, 0", UTC_OFFSET, 40);
+  wifiManager.addParameter(&timezoneparameter);
+
+  wifiManager.autoConnect();
+
   pinMode(TRIGGER, OUTPUT);
   digitalWrite(TRIGGER, HIGH);
-
-  WiFi.begin (ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(1000);
-    Timeout--;
-    if (Timeout==0){
-      Serial.println("\nWiFi Timeout");
-      break;
-    }
-  }
 
   dac_output_enable(DAC_CHANNEL_1);
   dac_output_enable(DAC_CHANNEL_2);
@@ -394,14 +436,14 @@ void setup()
 
   char *dstAbbrev;
   time_t now = dstAdjusted.time(&dstAbbrev);
-  struct tm * timeinfo = localtime (&now);
+  struct tm *timeinfo = localtime(&now);
 
   Serial.println();
-  Serial.printf("Current time: %d:%d:%d\n", timeinfo->tm_hour%12, timeinfo->tm_min,timeinfo->tm_sec);
+  Serial.printf("Current time: %d:%d:%d\n", timeinfo->tm_hour % 12, timeinfo->tm_min, timeinfo->tm_sec);
 
-//  // calculate for time calculation how much the dst class adds.
-//  time_t dstOffset = UTC_OFFSET * 3600 + dstAdjusted.time(nullptr) - now;
-//  Serial.printf("Time difference for DST: %d\n", dstOffset);
+  //  // calculate for time calculation how much the dst class adds.
+  //  time_t dstOffset = UTC_OFFSET * 3600 + dstAdjusted.time(nullptr) - now;
+  //  Serial.printf("Time difference for DST: %d\n", dstOffset);
 
   previousMillis = currentMillis;
 }
@@ -409,26 +451,26 @@ void setup()
 // End setup
 //*****************************************************************************
 
-
-
 //*****************************************************************************
 // loop
 //*****************************************************************************
 
-void loop() {
+void loop()
+{
 
   currentMillis = millis();
 
-  if (currentMillis - previousMillis >= interval) {
+  if (currentMillis - previousMillis >= interval)
+  {
     previousMillis = currentMillis;
     getNtpTime();
   }
 
   char *dstAbbrev;
   time_t now = dstAdjusted.time(&dstAbbrev);
-  struct tm * timeinfo = localtime (&now);
+  struct tm *timeinfo = localtime(&now);
 
-  int h = timeinfo->tm_hour%12;
+  int h = timeinfo->tm_hour % 12;
   int m = timeinfo->tm_min;
   int s = timeinfo->tm_sec;
 
@@ -444,16 +486,16 @@ void loop() {
   //Serial.println("Diagonals");                        //2 to back trace
   //for (i=0; i < 2000; i++) PlotTable(DialData,sizeof(DialData),0x00,0,0);
 
-  PlotTable(DialData,sizeof(DialData),0x00,1,0);      //2 to back trace
-  PlotTable(DialDigits12,sizeof(DialDigits12),0x00,1,0);//2 to back trace
-  PlotTable(HrPtrData, sizeof(HrPtrData), 0xFF,0,(9*(5*h+m/12)));  // 9*h
-  PlotTable(MinPtrData,sizeof(MinPtrData),0xFF,0,9*m);  // 9*m
-  PlotTable(SecPtrData,sizeof(SecPtrData),0xFF,0,5*s);  // 5*s
+  PlotTable(DialData, sizeof(DialData), 0x00, 1, 0);                        //2 to back trace
+  PlotTable(DialDigits12, sizeof(DialDigits12), 0x00, 1, 0);                //2 to back trace
+  PlotTable(HrPtrData, sizeof(HrPtrData), 0xFF, 0, (9 * (5 * h + m / 12))); // 9*h
+  PlotTable(MinPtrData, sizeof(MinPtrData), 0xFF, 0, 9 * m);                // 9*m
+  PlotTable(SecPtrData, sizeof(SecPtrData), 0xFF, 0, 5 * s);                // 5*s
 
-  #if defined EXCEL
-    while(1);
-  #endif
-
+#if defined EXCEL
+  while (1)
+    ;
+#endif
 }
 
 // End loop
